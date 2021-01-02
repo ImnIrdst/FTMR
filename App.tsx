e237345fd27487440e0384e3dc45cc31b6eb7ae4
iman <imn.irdst@gmail.com>
2021-01-02 23:39:56 +0330

    Fix status bar theme

--- App.tsx
+++ App.tsx
@@ -5,10 +5,8 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.body}>
-                Open up App.tsx asdasdto star st working on your app!
-            </Text>
-            <StatusBar style="auto" />
+            <Text style={styles.body}>00:00</Text>
+            <StatusBar style="light" backgroundColor="black" />
         </SafeAreaView>
     );
 }
@@ -16,12 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#000",
+        backgroundColor: "black",
         alignItems: "center",
         justifyContent: "center",
     },
     body: {
-        fontSize: 16,
+        fontSize: 32,
         color: "#eee",
     },
 });

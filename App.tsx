10806fe1c14706f04500b4422b59c38a3f533ffe
iman <imn.irdst@gmail.com>
2021-01-05 01:52:26 +0330

    Add simple timer logic

--- App.tsx
+++ App.tsx
@@ -1,23 +1,78 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, SafeAreaView, StyleSheet, Text, NativeModules } from "react-native";
+import {
+    Button,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    NativeModules,
+} from "react-native";
 
-const {FtmrNotificationModule} = NativeModules
+const { FtmrNotificationModule } = NativeModules;
 
-export default function App() {
+interface MainScreenState {
+    currentTime: number;
+}
 
-    const onSendNotificationPressed = () => {
-        console.log("Here")
-        FtmrNotificationModule.showNotification("Ftmr", "Times up!")
-    }
+interface MainScreenProps {}
 
-    return (
+const currentTimeMilies = () => {
+    return new Date().getTime();
+};
+
+const addLeadingZeros = (num: number) => {
+    let str = num.toString();
+    if (str.length == 1) return "0" + str;
+    else return str;
+};
+
+const formatEpoch = (epoch: number) => {
+    let seconds = Math.round(epoch / 1000);
+    let minutes = Math.round(seconds / 60);
+    return `${addLeadingZeros(minutes)}:${addLeadingZeros(seconds % 60)}`;
+};
+
+export default class App extends React.Component {
+    intervalId: any;
+    startTime = -1;
+    duration = 5000;
+
+    state = {
+        currentTime: this.duration,
+    };
+
+    sendNotification = () => {
+        FtmrNotificationModule.showNotification("Ftmr", "Times up!");
+    };
+
+    startTimer = () => {
+        this.startTime = currentTimeMilies();
+        this.setState({ currentTime: this.duration });
+
+        clearInterval(this.intervalId);
+
+        this.intervalId = setInterval(() => {
+            this.setState({ currentTime: this.state.currentTime - 1000 });
+            if (currentTimeMilies() > this.startTime + this.duration) {
+                this.sendNotification();
+                clearInterval(this.intervalId);
+            }
+        }, 1000);
+    };
+
+    render = () => (
         <SafeAreaView style={styles.container}>
             <StatusBar style="light" backgroundColor="black" />
-            
-            <Text style={styles.body}>00:00</Text>
-            
-            <Button title="Send Notification" onPress={onSendNotificationPressed} color="darkorange"/>
+
+            <Text style={styles.body}>
+                {formatEpoch(this.state.currentTime)}
+            </Text>
+
+            <Button
+                title="Send Notification"
+                onPress={this.startTimer}
+                color="darkorange"
+            />
         </SafeAreaView>
     );
 }
@@ -28,7 +83,7 @@ const styles = StyleSheet.create({
         backgroundColor: "black",
         alignItems: "center",
         justifyContent: "flex-end",
-        paddingBottom: 64
+        paddingBottom: 64,
     },
     body: {
         textAlignVertical: "center",
